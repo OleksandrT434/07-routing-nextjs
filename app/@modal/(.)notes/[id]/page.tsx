9b7ca73ebd9d347cc19d466ea987df32f4748240
Aleksandr Tkachenko <prosto.batman1234@gmail.com>
2025-08-13 20:31:48 +0200

    fix

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,14 +1,15 @@
-import { fetchNoteById } from '@/lib/api';
-import type { Note } from '@/types/note';
-import NoteModalClient from './NotePreview';
+  import { fetchNoteById } from '@/lib/api';
+  import type { Note } from '@/types/note';
+  import NoteModalClient from './NotePreview';
+  
 
-type Props = {
-  params: { id: string };
-};
+  type Props = {
+    params: { id: string };
+  };
 
-export default async function NoteModalPage({ params }: Props) {
-  const { id } = params;
-  const note: Note = await fetchNoteById(id);
+  export default async function NoteModalPage({ params }: Props) {
+    const { id } = params;
+    const note: Note = await fetchNoteById(id);
 
-  return <NoteModalClient note={note} />;
-}
+    return <NoteModalClient note={note} />;
+  }
