cdc88e37fae6fff9467ba4c25384b4d45e0fa34e
Aleksandr Tkachenko <prosto.batman1234@gmail.com>
2025-08-13 21:07:22 +0200

    15

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,15 +1,22 @@
-  import { fetchNoteById } from '@/lib/api';
-  import type { Note } from '@/types/note';
-  import NoteModalClient from './NotePreview';
-  
+import { fetchNoteById } from '@/lib/api';
+import type { Note } from '@/types/note';
+import NoteModalClient from './NotePreview';
 
-  interface PageProps {
-    params: { id: string };
+interface PageProps {
+  note: Note;
+}
+
+export async function getProps(context: { params: { id: string } }) {
+  const { id } = context.params;
+  const note: Note = await fetchNoteById(id);
+
+  return {
+    props: {
+      note,
+    },
   };
+}
 
-  export default async function Page({ params }: PageProps) {
-    const { id } = await params;
-    const note: Note = await fetchNoteById(id);
-
-    return <NoteModalClient note={note} />;
-  }
+export default function Page({ note }: PageProps) {
+  return <NoteModalClient note={note} />;
+}
